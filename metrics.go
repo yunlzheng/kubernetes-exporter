9d28dfe1eaad7a3dae0661c7a378f7f540f694de
yunl.zheng <yunl.zheng@wise2c.com>
2017-06-30 10:25:01 +0800

    update

--- metrics.go
+++ metrics.go
@@ -21,6 +21,13 @@ func addMetrics() map[string]*prometheus.GaugeVec {
 			Help:      "status of deployment reported by kubernates",
 		}, []string{"name", "namespace"})
 
+	gaugeVecs["stacks"] = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "stack_status",
+			Help:      "status of stack reported by kubernates",
+		}, []string{"name", "namespace"})
+
 	gaugeVecs["pods"] = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Namespace: namespace,
